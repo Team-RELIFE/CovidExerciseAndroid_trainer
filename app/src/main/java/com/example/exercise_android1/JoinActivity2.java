730361c68c9e7580f35c7a15f5db8d87142b9359
MinJi <jeongminji4490>
2021-04-01 19:33:57 +0900

    트레이너용! 메인 화면이랑 스플래시 수정, 회원가입에서 갤러리로 이동해 사진 추가하는 버튼 추가까지만!했음

--- app/src/main/java/com/example/exercise_android1/JoinActivity2.java
+++ app/src/main/java/com/example/exercise_android1/JoinActivity2.java
@@ -1,15 +1,23 @@
 package com.example.exercise_android1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.InputStream;
 
 public class JoinActivity2 extends AppCompatActivity {
 
     ImageButton joinOkBtn; /*회원가입 완료 버튼*/
+    ImageButton galleryBtn; /*갤러리 이동 버튼*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,7 @@ public class JoinActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_join2);
 
         joinOkBtn=(ImageButton)findViewById(R.id.JoinOkBtn);
+        galleryBtn=(ImageButton)findViewById(R.id.galleryBtn);
 
         /*jsp와 연동하여 db에 회원정보 저장하는 알고리즘 필요*/
         /*회원가입 성공 토스트메세지 필요*/
@@ -28,6 +37,40 @@ public class JoinActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        /*클릭 시 갤러리로 이동*/
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    /*갤러리에서 선택한 이미지를 버튼에 띄움*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1){
+            if(resultCode==RESULT_OK){
+                try {
+                    InputStream in=getContentResolver().openInputStream(data.getData());
+
+                    Bitmap img= BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    galleryBtn.setImageBitmap(img);
+                }catch (Exception e){
+                }
+            }
+            else if (resultCode==RESULT_CANCELED){
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
